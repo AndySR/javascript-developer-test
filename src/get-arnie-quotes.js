0ed8bb1df03b43c71b6d98dcdaa135c2c6c1df91
Andy <yiensuen@gmail.com>
2025-11-11 12:01:06 +1100

    feat:Smokeball JavaScript Coding Challenge

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,7 +2,20 @@ const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+  const promises = urls.map(async (url) => {
+    try {
+      const response = await httpGet(url);
+      const { message } = JSON.parse(response.body);
+
+      return response.status === 200
+        ? { 'Arnie Quote': message }
+        : { FAILURE: message };
+    } catch (error) {
+      return { FAILURE: error.message };
+    }
+  });
+  const results = await Promise.all(promises);
+  return results;
 };
 
 module.exports = {
